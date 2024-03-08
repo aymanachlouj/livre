f08c33987b6a86f600f975afd3ee4163247c061f
aymanachlouj <aymanachlouj2000@gmail.com>
2024-03-08 17:20:21 +0100

    ajouter serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Livre
+
+class LivreSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Livre
+        fields = ['titre','auteur','description','image']

7f8657619827fe151424da1390b7f8f052968764
aymanachlouj <aymanachlouj2000@gmail.com>
2024-03-08 19:00:32 +0100

    Library init

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Livre
 
-class LivreAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-
-admin.site.register(Livre, LivreAdmin)
+# Register your models here.
--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Livre
-
-class LivreSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Livre
-        fields = ['titre','auteur','description','image']
